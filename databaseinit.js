5771a7f56513c47ceeab0635535ae305c95b800e
Devin F <devinpfranchino@gmail.com>
2025-03-26 18:39:19 -0400

    Database Created

--- databaseinit.js
+++ databaseinit.js
@@ -1,13 +1,32 @@
-const betterSQL3 = require('better-sqlite3');
-const initiated = 0;
-const db; 
-function initDb() {
-    if (initiated == 0) {
-        db = new Database('studentApp.db');
-        initiated = 1;
-    }
-}
-function runDB() {
-
-}
+const Database = require('better-sqlite3');
+const db = Database('studentAppDB.db');
+//creating accounts Table
+db.exec(`
+    CREATE TABLE IF NOT EXISTS accounts(
+        accountID INTEGER PRIMARY KEY AUTOINCREMENT,
+        email VARCHAR(255) NOT NULL,
+        creationDate DATE NOT NULL,
+        viewPerms BOOLEAN NOT NULL,
+        postPerms BOOLEAN NOT NULL,
+        adminPerms BOOLEAN NOT NULL,
+        postCount INT,
+        deletedAccount BOOLEAN NOT NULL
+    );`
+);
+//creating accounts Table
+db.exec(`
+    CREATE TABLE IF NOT EXISTS posts (
+        postID INTEGER PRIMARY KEY AUTOINCREMENT,
+        accountID INT, 
+        postDate DATE NOT NULL,
+        Title nvarchar(255) NOT NULL,
+        postDescription nvarchar(3000),
+        image MEDIUMBLOB,
+        video LARGEBLOB,  
+        hasBeenReported BOOLEAN NOT NULL,
+        hasBeenDeleted BOOLEAN NOT NULL,
+        FOREIGN KEY (accountID) REFERENCES accounts(accountID)
+    );`
+);
+db.close();
 
