1338df1d9526a323a833cba065e80f084ea90132
Russell Abraham <russabejr@gmail.com>
2025-04-02 23:33:34 -0400

    Added a search bar to the Following List and made the Following List an unordered list to make this work!

--- public/scripts/followinglist.js
+++ public/scripts/followinglist.js
@@ -6,6 +6,14 @@
         const createPstBtn = document.getElementById("createPstBtn");
         const fllwLstBtn = document.getElementById("fllwLstBtn");
 
+        // Search bar
+        const searchInput = document.createElement("input");
+        searchInput.type = "text";
+        searchInput.id = "searchInput";
+        searchInput.placeholder = "Search...";
+        searchInput.addEventListener("keyup", filterList);
+        followingListContainer.appendChild(searchInput);
+
         // Testing/Example users
         const followedUsers = [
             { username: "RussAbeJr", fullName: "Russell Abraham", profilePic: "../assets/images/blank_pfp.png" },
@@ -18,7 +26,7 @@
             { username: "mrTesting", fullName: "Test Person", profilePic: "../assets/images/blank_pfp.png" },
             { username: "missExample", fullName: "Miss Example", profilePic: "../assets/images/blank_pfp.png" },
             { username: "idk", fullName: "Isaiah Doesnt Know", profilePic: "../assets/images/blank_pfp.png" },
-            { username: "ihaveareallylongnamewhywouldieverdothis", fullName: "Why Did My Parents Give Me Such A Long Name", profilePic: "../assets/images/blank_pfp.png" }
+            { username: "ihaveareallyreallyreallylongnamewhywouldieverdothis", fullName: "Why Did My Parents Give Me Such A Long Name", profilePic: "../assets/images/blank_pfp.png" }
         ];
 
         // Function to render the followed users list
@@ -28,15 +36,40 @@
                 return;
             }
 
-            followingListContainer.innerHTML = followedUsers.map(user => `
-                <div class="followed-user">
-                    <img src="${user.profilePic}" alt="${user.fullName}" class="profile-pic">
-                    <div class="user-info">
-                        <h3>${user.fullName}</h3>
-                        <p>@${user.username}</p>
-                    </div>
-                </div>
+            // Create the unordered list
+            const ul = document.createElement("ul");
+            ul.id = "followingList";
+
+            // Populate list items
+            ul.innerHTML = followedUsers.map(user => `
+                <li class="followed-user">
+                    <a href="#" class="user-link">
+                        <img src="${user.profilePic}" alt="${user.fullName}" class="profile-pic">
+                        <div class="user-info">
+                            <h3>${user.fullName}</h3>
+                            <p>@${user.username}</p>
+                        </div>
+                    </a>
+                </li>
             `).join("");
+
+            followingListContainer.appendChild(ul);
+        }
+
+        // Search filter function
+        function filterList() {
+            const input = document.getElementById("searchInput").value.toUpperCase();
+            const ul = document.getElementById("followingList");
+            const li = ul.getElementsByTagName("li");
+
+            for (let i = 0; i < li.length; i++) {
+                const userName = li[i].getElementsByTagName("h3")[0].textContent.toUpperCase();
+                if (userName.indexOf(input) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
         }
 
         // Back button event listener
