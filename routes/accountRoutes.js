5dbf42c121c23f5efd3994f0a0f347d0f3d92c45
Devin F <devinpfranchino@gmail.com>
2025-04-23 16:09:18 -0400

    Fixed the file navigation

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,7 +1,7 @@
 //Dependencies
 const express = require('express');
-const { verifyAccessToken } = require('./auth/authJWT');
-const accountController = require('././controllers/accountController');
+const { verifyAccessToken } = require('../auth/authJWT');
+const accountController = require('../controllers/accountController');
 //Static Fields
 const router = express.Router();
 
