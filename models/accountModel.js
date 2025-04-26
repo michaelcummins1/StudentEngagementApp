e279783961d34e707fd1383ee1589160a7d6441c
Devin F <devinpfranchino@gmail.com>
2025-04-26 00:15:35 -0400

    Implemented account registration: controller and model, rebuilt DB (again), updated variables in authJWT, added header to registration.html

--- models/accountModel.js
+++ models/accountModel.js
@@ -1,13 +1,21 @@
 const db = require('../databaseinit');
-// We need an encryption module (pls find)
-//Creation date also gonna need some kind of magic.
-exports.createAccount = async (email, password, creationDate) => {
-    //Encrypt PW before committing to DB
-    db.run(
-        'INSERT INTO accounts ()',
-        [email, password, creationDate],
-        function (err){
-            callback(err, this?.lastID);
-        }
-    );
+const bcrypt = require('bcrypt');
+
+exports.createAccount = async (email, password, creationDate, callback) => {
+new Promise(function (resolve,reject){
+    insertAccount = db.prepare('INSERT INTO accounts (email, password, creationDate, deletedAccount) VALUES (?,?,?,FALSE)');
+    insertAccount.run(email,password,creationDate);
+    const lastInsertID = db.prepare('select last_insert_rowid()').get().last_insert_rowid;
+    //Go back to Controller
+    console.log("Account has been added to the DB");
+    if(lastInsertID){
+    resolve(lastInsertID)
+    } else {reject();}
+})
+.then(function(accountID){
+    callback(null,accountID);
+})
+.catch(function(){
+    callback("error",null)
+});
 };
