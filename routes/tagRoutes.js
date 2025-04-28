84e042d014552dae7923079d592f2bff0baab2fa
Michael Cummins <mjcrocks1@gmail.com>
2025-04-28 16:06:07 -0400

    Fixed database requirement in tag routes

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -4,6 +4,6 @@ const tagController = require('../controllers/tagController');
 const accountController = require('../controllers/accountController');
 const postController = require('../controllers/postController');
 const { verifyAccessToken } = require('../auth/authJWT');
-const db = require('../db');
+const db = require('../databaseinit');
 
 module.exports = router;
