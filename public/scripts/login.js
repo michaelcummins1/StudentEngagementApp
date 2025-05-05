f8c5de88584c8f2fb391dfc26554b722890af66c
Devin F <devinpfranchino@gmail.com>
2025-05-05 18:19:47 -0400

    Logging in Implemented (DOES NOT REDIRECT TO HOME PAGE THOUGH!!!) Need : Logging out / redirects

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -3,11 +3,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const homePgBtn = document.getElementById("homePgBtn");
     const createAccountBtn = document.getElementById("createAccountBtn");
-
+    const logInBtn = document.getElementById("logInBtn");
     homePgBtn.addEventListener("click", () => {
         window.location.href = "/pages/mainpage.html";
     });
     createAccountBtn.addEventListener("click", () => {
         window.location.href = "/pages/registration.html";
+    });
+    logInBtn.addEventListener("click", () => {
+        window.location.href = "/pages/mainpage.html";
     });
 })
