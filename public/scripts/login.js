f18b05ec4e0a9b091e7e6080ff9c8f4c65150009
Michael Cummins <mjcrocks1@gmail.com>
2025-05-08 16:13:52 -0400

    removed the move to main screen button from the login page

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,13 +1,9 @@
 // Listener for the page upon load
 
 document.addEventListener("DOMContentLoaded", function () {
-    const homePgBtn = document.getElementById("homePgBtn");
     const createAccountBtn = document.getElementById("createAccountBtn");
     const logInBtn = document.getElementById("logInBtn");
     const forgotInfoBtn = document.getElementById("forgotInfoBtn");
-    homePgBtn.addEventListener("click", () => {
-        window.location.href = "/pages/mainpage.html";
-    });
     createAccountBtn.addEventListener("click", () => {
         window.location.href = "/pages/registration.html";
     });
